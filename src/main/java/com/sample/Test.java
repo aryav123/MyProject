b014e6beaba1502e8a7f3594157ad3d519d2e097
aryav123 <arya74331@gmail.com>
2024-10-01 20:33:57 +0530

    first commit

---
+++ src/main/java/com/sample/Test.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=15;
+		System.out.println("A is:"+a);
+		System.out.println("B is:"+b);
+
+
+	}
+}
---
+++ src/test/java/com/sample/LoginTest.java
@@ -0,0 +1,27 @@
+package com.sample;
+
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void login_Test() {
+//		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();            /** Maximize window**/
+//	    driver.manage().deleteAllCookies();             /** Delete all cookies**/
+//	    driver.get("https://www.google.com/");
+//	    driver.quit();
+		
+		int a,b,sum;
+		a=10;
+		b=15;
+		sum=a+b;
+		System.out.println("Sum of A and B is:"+sum);
+	}
+
+}
